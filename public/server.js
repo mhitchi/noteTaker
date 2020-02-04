ad08d01817e2c4785c26d04261e2dd3733cde449
mhitchi <mayhitchings@gmail.com>
2020-02-04 17:39:04 -0500

    boilerplate added into server.js

---
+++ public/server.js
@@ -0,0 +1,17 @@
+//dependencies
+var express = require("express");
+var path = require("path");
+var app = express();
+
+//set up initial port
+var PORT = process.env.PORT || 8080;
+//set up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routing
+
+//start server listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
